d9a1d0718e3597bec3e3098633ab781e079ff168
usercsie <usercsie@gmail.com>
2021-01-09 23:59:01 +0800

    Say hello world by function and class.

--- 01_HelloWorld/HelloWorld.py
+++ 01_HelloWorld/HelloWorld.py
@@ -1,1 +1,16 @@
 print('Hello world')
+
+
+def SayHelloByFunction():
+    print('Hello world by function')
+
+
+class SayHello:
+    def Say(self):
+        print('Hello world by class')
+
+
+SayHelloByFunction()
+
+person = SayHello()
+person.Say()
