23bf73c78e46233d163af7a33690fbcfa142425e
usercsie <usercsie@gmail.com>
2021-01-10 11:00:54 +0800

    Add SayHelloAtInit_Derived Add SayHelloAtInit

--- 01_HelloWorld/HelloWorld.py
+++ 01_HelloWorld/HelloWorld.py
@@ -10,7 +10,27 @@ class SayHello:
         print('Hello world by class')
 
 
+class SayHelloAtInit:
+    def __init__(self, words):
+        self.words = words
+
+    def Say(self):
+        print(self.words)
+
+
+class SayHelloAtInit_Derived(SayHelloAtInit):
+    def Say(self):
+        print(self.words, ' - Derived')
+
+
+    # ----------------------------------------------------------------------------------
 SayHelloByFunction()
 
 person = SayHello()
+person.Say()
+
+person = SayHelloAtInit('Hello world by class __init__')
+person.Say()
+
+person = SayHelloAtInit_Derived('Hello world by class __init__')
 person.Say()
